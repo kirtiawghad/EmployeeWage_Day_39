485fca66c2348a14f8d025f33db8dcfae7833d64
kirtimadam <104613258+kirtimadam@users.noreply.github.com>
2023-09-09 19:33:00 +0530

    Added:uc1

---
+++ src/com/bl/EmployeeWage.js
@@ -0,0 +1,7 @@
+let isPresent = 1;
+let empcheckrandom = Math.floor(Math.random()*2);
+if(empcheckrandom == isPresent){
+    console.log("Employee is present");
+}else{
+    console.log("Employee not present");
+}
